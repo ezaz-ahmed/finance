cef4b7eb279dd67d2580e0ea9411f5276527ea87
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-06-05 02:13:29 +0600

    feat: introduce zod validation

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
 app.use("*", logger());
@@ -11,5 +12,7 @@ app.get("/healthcheck", (c) => {
     timestamp: new Date().toISOString(),
   });
 });
+
+app.route("/api/expenses", expensesRoute);
 
 export default app;
--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,6 +1,18 @@
 import { Hono } from "hono";
+import { z } from "zod";
 
 export const expensesRoute = new Hono();
+
+type Expense = {
+  id: number;
+  title: string;
+  amount: number;
+};
+
+const createExpenseSchema = z.object({
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+});
 
 expensesRoute
   .get("/", (c) => {
@@ -8,6 +20,12 @@ expensesRoute
       expenses: [],
     });
   })
-  .post("/", (c) => {
+  .post("/", async (c) => {
+    const data = await c.req.json();
+
+    const expense = createExpenseSchema.parse(data);
+
+    console.log({ expense });
+
     return c.json({});
   });
