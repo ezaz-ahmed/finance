2e9505216b92200e38361ceb7080ac0698f2b186
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-06-05 01:26:24 +0600

    remove: everything

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_TITLE = "BroadScript";
-export const SITE_DESCRIPTION = "Articles, Stories & Tutorials for Tech People";
-export const HOMEPAGE_ARTICLE_LIMIT = 6;
-export const ARTICLES_PER_PAGE = 6;
