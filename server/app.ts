57ac9959253986650ed5fc07b707804ec05ac020
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-06-10 16:40:04 +0600

    feat: utilize for production & development

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
 import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
@@ -14,5 +15,8 @@ app.get("/healthcheck", (c) => {
 });
 
 app.route("/api/expenses", expensesRoute);
+
+app.get("*", serveStatic({ root: "./client/dist" }));
+app.get("*", serveStatic({ path: "./client/dist/index.html" }));
 
 export default app;
