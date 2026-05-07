b4a3ee2092300b6c3bbaf4b83967cf10038ca561
Ezaz Ahmed <ezaznahmed@gmail.com>
2026-05-07 15:18:35 +0600

    chore: remove client and server codebase along with configuration files

--- client/src/routes/create-expense.lazy.tsx
+++
@@ -1,9 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-export const Route = createLazyFileRoute("/create-expense")({
-  component: CreateExpnese,
-});
-
-function CreateExpnese() {
-  return <div>Lorem ipsum dolor sit amet.</div>;
-}
